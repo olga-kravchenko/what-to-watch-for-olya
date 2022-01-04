f9ec5b1f737eac7205153986e43c4008068a0493
Olga <olga19972010@yandex.by>
2022-01-04 14:13:27 +0300

    2.1 Described routing in the application.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,42 @@
 import React from "react";
-import Main from "../main/main";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import Main from "../main/main";
+import Login from "../login/login";
+import MyList from "../my-list/my-list";
+import Film from "../film/film";
+import AddReview from "../add-review/add-review";
+import Player from "../player/player";
+import PageNotFound from "../page-not-found/page-not-found";
 
 const App = ({genre, year}) => {
-  return (<Main genre={genre} year={year}/>);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" >
+          <Main genre={genre} year={year}/>
+        </Route>
+        <Route path="/login" >
+          <Login/>
+        </Route>
+        <Route path="/mylist" exact>
+          <MyList/>
+        </Route>
+        <Route path="/films/:id" exact>
+          <Film/>
+        </Route>
+        <Route path="/films/:id/review" exact>
+          <AddReview/>
+        </Route>
+        <Route path="/player/:id" exact>
+          <Player/>
+        </Route>
+        <Route>
+          <PageNotFound/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
