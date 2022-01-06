3d013bb3873294a5631b7606f01cc895cd97cfce
Olga <olga19972010@yandex.by>
2022-01-06 19:52:37 +0300

    3.1 Added review-form component.

--- src/components/add-review/add-review.jsx
+++ src/components/add-review/add-review.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../header/header";
 import {useParams} from "react-router-dom";
 import ReviewStar from "../review-star/review-star";
+import ReviewForm from "../review-form/review-form";
 
 const AddReview = ({films}) => {
   const {id} = useParams();
@@ -9,7 +10,6 @@ const AddReview = ({films}) => {
   const film = films[index];
   const {name, background_image, poster_image} = film;
 
-  const starQuantity = Array.from({length: 10}, (_, index) => index + 1);
   return (
     <section className="movie-card movie-card--full">
       <div className="movie-card__header">
@@ -27,24 +27,7 @@ const AddReview = ({films}) => {
         </div>
       </div>
 
-      <div className="add-review">
-        <form action="#" className="add-review__form">
-          <div className="rating">
-            <div className="rating__stars">
-              {starQuantity.map((number, i) => (
-                <ReviewStar number={number} key={i}/>))}
-            </div>
-          </div>
-
-          <div className="add-review__text">
-            <textarea className="add-review__textarea" name="review-text" id="review-text"
-              placeholder="Review text"/>
-            <div className="add-review__submit">
-              <button className="add-review__btn" type="submit">Post</button>
-            </div>
-          </div>
-        </form>
-      </div>
+     <ReviewForm/>
 
     </section>
   );
---
+++ src/components/review-form/review-form.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ReviewStar from "../review-star/review-star";
+import {useState} from "react";
+
+const ReviewForm = () => {
+  const [userForm, setUserForm] = useState({
+    "review-text": ``,
+    "rating": ``
+  });
+
+  const handleFieldChange = (evt) => {
+    const {name, value} = evt.target;
+    setUserForm({...userForm, [name]: value});
+  };
+
+  const starQuantity = Array.from({length: 10}, (_, index) => index + 1);
+  return (
+    <div className="add-review">
+      <form action="#" className="add-review__form">
+        <div className="rating">
+          <div className="rating__stars"  onChange={handleFieldChange}>
+            {starQuantity.map((number, i) => (
+              <ReviewStar number={number} key={i}/>))}
+          </div>
+        </div>
+
+        <div className="add-review__text">
+            <textarea className="add-review__textarea" name="review-text" id="review-text"
+                      placeholder="Review text"  onChange={handleFieldChange}/>
+          <div className="add-review__submit">
+            <button className="add-review__btn" type="submit">Post</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ReviewForm;
