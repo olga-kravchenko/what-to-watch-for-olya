e7edb5e6ef1f77b32fcbe9f9831621ffee203fde
Olga <olga19972010@yandex.by>
2022-01-07 12:02:15 +0300

    3.1 Added state for film-card.

--- src/components/films/films.jsx
+++ src/components/films/films.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import FilmCard from "../film-card/film-card";
 import PropTypes from "prop-types";
+import {useState} from "react";
 
 const Films = ({films}) => {
+  const [activeFilmId, setActiveFilmId] = useState({activeFilmId: ``});
   return (
-    <div className="catalog__movies-list">
-      {films.map((film, i) => <FilmCard film={film} key={i} />)}
+    <div className="catalog__movies-list"
+         onMouseOver={({target}) => {
+           if (target.className === `catalog__movies-list`) {
+             return;
+           }
+           const hoverFilm = target.closest(`article`).id;
+           setActiveFilmId({...activeFilmId, activeFilmId: hoverFilm});
+         }}>
+
+      {films.map((film, i) => <FilmCard film={film} key={i}/>)}
     </div>);
 };
 
