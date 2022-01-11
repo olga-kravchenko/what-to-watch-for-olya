9792e47826ac170777e7e3367bf5e1e53860a730
Olga <olga19972010@yandex.by>
2022-01-11 17:39:40 +0300

    5.1 Connected redux to project.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import {films} from "./mocks/films";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import reducer from "./store/reducer";
 
 const genre = `Drama`;
 const year = 2014;
 
-ReactDOM.render(<App films={films} genre={genre} year={year}/>, document.querySelector(`#root`));
+const store = createStore(reducer, composeWithDevTools());
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App films={films} genre={genre} year={year}/>
+    </Provider>,
+    document.querySelector(`#root`));
